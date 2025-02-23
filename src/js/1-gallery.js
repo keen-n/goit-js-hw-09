6df92575f4141cd48078b733f8670ec833a5ba10
Inna Karpenko <ferqroyer@gmail.com>
2025-02-24 00:29:52 +0200

    final gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -68,9 +68,6 @@ const images = [
 ];
 
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 const gallery = document.querySelector('.gallery');
 
 function imageTemplate(image) { 
